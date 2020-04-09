8d78689c3f22ccc20a3ea0cf71fecc4504f7616d
towa-hi <yamyamasunday@gmail.com>
2020-04-08 22:42:00 -0700

    editor manager

---
+++ Assets/Scripts/EditorManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorManager : Singleton<EditorManager> {
+    public LevelData levelData;
+    public List<BlockObject> blockList;
+    public List<BlockObject> selectedList;
+    public List<MobObject> mobList;
+    
+    public Vector3 mousePos;
+    public Vector3 oldMousePos;
+    
+    void Awake() {
+        
+    }
+}
